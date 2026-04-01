59b96201a41128a8b43f95d5c2d100eee1b44a76
fabiopegini <fipegini@gmail.com>
2026-04-01 20:02:04 +0200

    Solution for exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,13 +2,27 @@ const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = (listOfBlogs) => {
-  const arrWithLikes = listOfBlogs.map(blog => blog.likes)
+const totalLikes = (blogs) => {
+  const arrWithLikes = blogs.map(blog => blog.likes)
 
   return arrWithLikes.reduce((acc, likes) => acc + likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let mostLikesYet = 0
+  let mostLikedBlog = undefined
+
+  for(const blog of blogs) {
+    if(mostLikesYet > blog.likes) continue
+    mostLikesYet = blog.likes
+    mostLikedBlog = { title: blog.title, author: blog.author, likes: blog.likes }
+  }
+
+  return mostLikedBlog
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
