9f7370b2df67f9ca267997190c25e24e5218c17b
fabiopegini <fipegini@gmail.com>
2026-04-01 19:31:36 +0200

    Solution for exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (listOfBlogs) => {
+  const arrWithLikes = listOfBlogs.map(blog => blog.likes)
+
+  return arrWithLikes.reduce((acc, likes) => acc + likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
