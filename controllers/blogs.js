00cbad752ca7e2ad94a01f3ea622a4e7eb3ee3bc
fabiopegini <fipegini@gmail.com>
2026-04-18 12:48:29 +0200

    Solution for exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,52 +1,71 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch(error => next(error))
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    return response.json(blogs)
+
+  } catch(error) {
+    next(error)
+  }
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
+blogsRouter.get('/:id', async (request, response, next) => {
   const { id } = request.params
 
-  Blog.findById(id)
-    .then(result => response.status(200).json(result))
-    .catch(error => next(error))
+  try {
+    const blog = await Blog.findById(id).populate('user', { username: 1, name: 1 })
+    return response.status(200).json(blog)
+
+  } catch(error) {
+    next(error)
+  }
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   if (!body.title || !body.author || !body.url) return response.status(400).send({ error: 'Missing data. A new blog must have a Title, an Author, and a URL. Also, may or may not have the amount of Likes it has' })
 
-  const newBlog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes ?? 0
-  })
+  try {
+    const users = await User.find({})
+    const user = users[0]
 
-  newBlog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
+    const newBlog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes ?? 0,
+      user: user.id
     })
-    .catch(error => next(error))
+
+    const savedBlog = await newBlog.save()
+
+
+    await User.findByIdAndUpdate(user.id, { blogs: [savedBlog._id, ...user.blogs] })
+
+    return response.status(201).json(savedBlog)
+    
+  } catch(error) {
+    next(error)
+  }
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
+blogsRouter.delete('/:id', async (request, response, next) => {
   const { id } = request.params
 
-  Blog.findByIdAndDelete(id)
-    .then(result => response.status(200).json({ success: `The blog ${result.title} was successfully deleted` }))
-    .catch(error => next(error))
+  try {
+    const result = await Blog.findByIdAndDelete(id)
+    return response.status(200).json({ success: `The blog ${result.title} was successfully deleted` })
+
+  } catch(error) {
+    next(error)
+  }
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put('/:id', async (request, response, next) => {
   const { id } = request.params
   const body = request.body
 
@@ -54,9 +73,13 @@ blogsRouter.put('/:id', (request, response, next) => {
 
   if(isEmpty(body)) return response.status(400).send({ error: 'You did not provide any updates for the resource' })
 
-  Blog.findByIdAndUpdate(id, body, { returnDocument: 'after' })
-    .then(result => response.status(200).json({success: `The blog: ${result.title}, was successfully modified`, result}))
-    .catch(error => next(error))
+  try {
+    const result = await Blog.findByIdAndUpdate(id, body, { returnDocument: 'after' })
+    return response.status(200).json({ success: `The blog: ${result.title}, was successfully modified`, result })
+
+  } catch(error) {
+    next(error)
+  }
 })
 
 module.exports = blogsRouter
--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,9 @@ const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (request, response, next) => {
   try {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { 
+      title: 1, author: 1, url: 1
+    })
     response.status(200).json(users)
 
   } catch(error) {
